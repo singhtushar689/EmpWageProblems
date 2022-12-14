d6be6f861d7ea784e49f320f1d7df4f2924d5650
singhtushar689 <singhtushar689@gmail.com>
2022-12-14 10:08:28 +0530

    Computing Daily Wage

--- CheckAttendance.cs
+++ CheckAttendance.cs
@@ -11,16 +11,25 @@ namespace EmpWageProblems
         public static void Attendance()
         {
             int present = 1;
+            int wagePerHour = 20;
+            int empHour = 0;
+            int dailyEmp = 0;
             Random random= new Random();
             int empcheck = random.Next(2);
             if(empcheck == 1)
             {
                 Console.WriteLine("Employee is present");
+                empHour = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                empHour = 0;
             }
+            dailyEmp = empHour * wagePerHour;
+            Console.WriteLine("Total Employee wage is " + dailyEmp);
+
+
         }
     }
 }
