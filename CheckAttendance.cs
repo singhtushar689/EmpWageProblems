af4cb50719bb3bd5e39d0cddf3edb7bd435fef37
singhtushar689 <singhtushar689@gmail.com>
2022-12-14 09:48:23 +0530

    Check Whether Employee is Present or not

---
+++ CheckAttendance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageProblems
+{
+    internal class CheckAttendance
+    {
+        public static void Attendance()
+        {
+            int present = 1;
+            Random random= new Random();
+            int empcheck = random.Next(2);
+            if(empcheck == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Employee Wage Problems");
+            CheckAttendance.Attendance();
         }
     }
 }
